a3ab97af135d87d4a38608a49930b138b74788ac
codesdhruv <dhruvcodeid@gmail.com>
2024-10-03 18:47:18 +0530

    Routing ep-04

--- src/app.js
+++ src/app.js
@@ -2,18 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Kunal", lastname: "kunal" });
+});
+app.post("/user", (req, res) => {
+  //databse logic
+  res.send("Data saved sucessfully in to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted sucessfully!!");
+});
+
 //Note: any route starting with below one i.e. "/" will be override by this route handler
 //      so use it carefully
 //      this route will overide "/test" and "/home" route handlers
+// app.use() will handle all the HTTP request
 app.use("/", (req, res) => {
   res.send("hello from dashboard");
-});
-
-app.use("/test", (req, res) => {
-  res.send("hello from test route");
-});
-app.use("/home", (req, res) => {
-  res.send("Namste from home");
 });
 
 app.listen(7777, () => {
