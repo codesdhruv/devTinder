6c9213b0842af8aea1b5045c0495743d997b35d4
codesdhruv <dhruvcodeid@gmail.com>
2024-10-04 14:52:40 +0530

    Muiltiple-routes-EP-04

--- src/app.js
+++ src/app.js
@@ -2,30 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.get("/a(bc)?d", (req, res) => {
-  res.send({ firstname: "Kunal", lastname: "kunal" });
-});
-
-//? REGEX use
-app.get(/z/, (req, res) => {
-  res.send({ firstname: "Kunal", lastname: "kunal" });
-});
-
-app.get(/.*fly$/, (req, res) => {
-  res.send({ firstname: "fly", lastname: "fly" });
-});
-
-//? Reading Query params - localhost:7777/user?userid=202&password=testing
-app.get("/users", (req, res) => {
-  console.log(req.query);
-  res.send({ firstname: "Kunal", lastname: "kunal" });
-});
-
-//? making route dynamically - localhost:7777/user/700/Akshay/Testing
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstname: "Kunal", lastname: "kunal" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // res.send("Rote Handler 1 !!");
+    next();
+    console.log("Route hamdler 1 !!");
+  },
+  (req, res, next) => {
+    console.log("Route hamdler 2 !!");
+    // res.send("Rote Handler 2 !!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route hamdler 3 !!");
+    // res.send("Rote Handler 3 !!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route hamdler 4 !!");
+    next();
+    //? the order of execution comes here after next() is called
+    res.send("Rote Handler 4 !!");
+  },
+  (req, res, next) => {
+    console.log("Route hamdler 5 !!");
+    // res.send("Rote Handler 5!!");
+  }
+);
 
 app.listen(7777, () => {
   console.log("server is sucessfully listening at port 7777....");
