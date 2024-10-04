15cabf92d2349f7740a51a69d9cf176a04391cbd
codesdhruv <dhruvcodeid@gmail.com>
2024-10-04 16:24:23 +0530

    middlreware-EP-04

--- src/app.js
+++ src/app.js
@@ -2,34 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    // res.send("Rote Handler 1 !!");
-    next();
-    console.log("Route hamdler 1 !!");
-  },
-  (req, res, next) => {
-    console.log("Route hamdler 2 !!");
-    // res.send("Rote Handler 2 !!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route hamdler 3 !!");
-    // res.send("Rote Handler 3 !!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Route hamdler 4 !!");
-    next();
-    //? the order of execution comes here after next() is called
-    res.send("Rote Handler 4 !!");
-  },
-  (req, res, next) => {
-    console.log("Route hamdler 5 !!");
-    // res.send("Rote Handler 5!!");
-  }
-);
+const { adminAuth, userAuth } = require("./middleware/auth");
+
+//? Handle Auth middleware for all GET, POST, ... request
+// app.use("/admin", (req, res, next) => {
+//   const token = "xyz";
+//   const isAuthorized = token === "xyz";
+//   if (!isAuthorized) {
+//     res.status(401).send("unauthorized acess");
+//   } else {
+//     next();
+//   }
+// });
+
+//? Directly passing middleware
+app.get("/user", userAuth, (req, res) => {
+  res.send("authorized user");
+});
+
+//? second way of passing middleware
+app.use("./admin", adminAuth);
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("authorized admin");
+});
+
+app.get("/admin/deleteAllData", (req, res) => {
+  res.send("authorized, data deleted !!");
+});
 
 app.listen(7777, () => {
   console.log("server is sucessfully listening at port 7777....");
